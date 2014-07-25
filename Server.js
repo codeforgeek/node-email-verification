f72af26023707cc096f9c39916a3d6e3e232cfdc
Shahid Shaikh <shahid@koding.info>
2014-07-25 20:45:16 +0530

    Version 1.0.0 upload

--- Server.js
+++ Server.js
@@ -9,9 +9,10 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
     service: "Gmail",
     auth: {
         user: "rwtc66@gmail.com",
-        pass: "#define(password.h)"
+        pass: "0N>m,4K6 (S<+ga"
     }
 });
+var rand,mailOptions,host,link;
 /*------------------SMTP Over-----------------------------*/
 
 /*------------------Routing Started ------------------------*/
@@ -20,10 +21,13 @@ app.get('/',function(req,res){
 	res.sendfile('index.html');
 });
 app.get('/send',function(req,res){
-	var mailOptions={
+        rand=Math.floor((Math.random() * 100) + 54);
+	host=req.get('host');
+	link="http://"+req.get('host')+"/verify?id="+rand;
+	mailOptions={
 		to : req.query.to,
-		subject : req.query.subject,
-		text : req.query.text
+		subject : "Please confirm your Email account",
+		html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"	
 	}
 	console.log(mailOptions);
 	smtpTransport.sendMail(mailOptions, function(error, response){
@@ -35,6 +39,28 @@ app.get('/send',function(req,res){
 		res.end("sent");
     	 }
 });
+});
+
+app.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+	console.log("Domain is matched. Information is from Authentic email");
+	if(req.query.id==rand)
+	{
+		console.log("email is verified");
+		res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+	}
+	else
+	{
+		console.log("email is not verified");
+		res.end("<h1>Bad Request</h1>");
+	}
+}
+else
+{
+	res.end("<h1>Request is from unknown source");
+}
 });
 
 /*--------------------Routing Over----------------------------*/
